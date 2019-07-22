07ac37623a665dd37638299f9bad5fd017e6bc17
Teryong <dthrcrpz@gmail.com>
2019-07-22 20:44:31 +0800

    vue now

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav>
+		<div class="container">
+			<div class="main">
+				<router-link to="/">dthrcrpz</router-link>
+			</div>
+			<div class="links">
+				<a href="javascript:void(0)">Link 1</a>
+				<a href="javascript:void(0)">Link 1</a>
+				<a href="javascript:void(0)">Link 1</a>
+			</div>
+		</div>
+	</nav>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div class="intro">
+			<div class="container">
+				<div class="photo"></div>
+				<h1>
+					Hi! I'm <span>Deither Corpuz</span>.
+					<br>
+					I'm a full-stack web developer.
+				</h1>
+				<a href="javascript:void">Learn More <span class="arrow">→</span></a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
