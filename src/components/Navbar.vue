494bc4f9245567f5017e52d50adfec305875b1e0
teryong <dthrcrpz@gmail.com>
2019-07-24 10:31:46 +0800

    added vue-scroll

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,10 +5,28 @@
 				<router-link to="/">dthrcrpz</router-link>
 			</div>
 			<div class="links">
-				<a href="javascript:void(0)">About</a>
-				<a href="javascript:void(0)">Link 1</a>
-				<a href="javascript:void(0)">Link 1</a>
+				<a href="javascript:void(0)" @click="scroll(navItem.target)" v-for="(navItem, key) in navItems" :key="key">{{ navItem.title }}</a>
 			</div>
 		</div>
 	</nav>
 </template>
+
+<script>
+	export default {
+		data () {
+			return {
+				navItems: [
+					{ title: 'About', target: '.about' },
+					{ title: 'Projects', target: '.projects' },
+				]
+			}
+		},
+		methods: {
+			scroll (target) {
+				this.$scrollTo(target, {
+					offset: -69
+				})
+			}
+		}
+	}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueParticles from 'vue-particles'
 import vClickOutside from 'v-click-outside'
 import Viewer from 'v-viewer'
+import VueScrollTo from 'vue-scrollto'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +15,19 @@ Vue.config.productionTip = false
 Vue.use(VueParticles)
 Vue.use(vClickOutside)
 Vue.use(Viewer)
+Vue.use(VueScrollTo, {
+	container: "body",
+	duration: 600,
+	easing: "ease",
+	offset: 0,
+	force: true,
+	cancelable: true,
+	onStart: false,
+	onDone: false,
+	onCancel: false,
+	x: false,
+	y: true
+})
 
 new Vue({
   router,
